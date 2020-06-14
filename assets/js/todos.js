f6e1a3be84d34efb45520a8b780d8a3706885354
Yangyin Xu <xy1@umbc.edu>
2020-06-14 09:58:23 -0400

    Added basic functionality and style

---
+++ assets/js/todos.js
@@ -0,0 +1,24 @@
+//Check Off Specific Todos By Clinking
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function () {
+	$(this).parent().fadeOut(500, function () {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type = 'text']").keypress(function (event) {
+	//enter key
+	if (event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+
+	}
+})
